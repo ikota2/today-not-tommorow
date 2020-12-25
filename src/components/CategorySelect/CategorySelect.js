5b001565bbfdee85b15897d45cdfb45480cddc34
Ivan <polyzay@gmail.com>
2020-12-25 10:22:04 +0300

    classnames renamed in CategorySelect

--- src/components/CategorySelect/CategorySelect.js
+++ src/components/CategorySelect/CategorySelect.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./categorySelect.css";
 
 function mapStateToProps(state) {
   return { categories: state.categories };
@@ -10,14 +11,14 @@ function CategorySelect_({ categories, value, onChange }) {
   return (
     <div className="categorySelect">
       <select
-        className="form__select"
+        className="categorySelect__select"
         value={value}
         onChange={(e) => onChange(e.target.value)}
       >
         {categories.map((category) => (
           <option
             value={category.id}
-            className="form__option"
+            className="categorySelect__option"
             key={category.id}
           >
             {category.description}
