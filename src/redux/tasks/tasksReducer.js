c7c160411dd3b2c99b94a30d2c7cd321e06d3200
Ivan <polyzay@gmail.com>
2021-01-12 08:59:53 +0300

    if statements to switch in taskReducer

--- src/redux/tasks/tasksReducer.js
+++ src/redux/tasks/tasksReducer.js
@@ -4,41 +4,40 @@ import { v4 as uuidv4 } from "uuid";
 const tasks = [];
 
 export function taskReducer(state = tasks, action) {
-  if (action.type === ADD_TASK) {
-    return [
-      ...state,
-      {
-        id: uuidv4(),
-        description: action.description,
-        categoryId: action.categoryId,
-        done: false,
-        date: new Date(),
-      },
-    ];
+  switch (action.type) {
+    case ADD_TASK:
+      return [
+        ...state,
+        {
+          id: uuidv4(),
+          description: action.description,
+          categoryId: action.categoryId,
+          done: false,
+          date: new Date(),
+        },
+      ];
+    case EDIT_TASK:
+      return state.map((task) =>
+        task.id === action.id
+          ? {
+              ...task,
+              description: action.description,
+              categoryId: action.categoryId,
+            }
+          : task
+      );
+    case TOGGLE_TASK:
+      return state.map((task) =>
+        task.id === action.id
+          ? {
+              ...task,
+              done: !task.done,
+            }
+          : task
+      );
+    case REMOVE_TASK:
+      return state.filter((task) => task.id !== action.id);
+    default:
+      return state;
   }
-  if (action.type === EDIT_TASK) {
-    return state.map((task) =>
-      task.id === action.id
-        ? {
-            ...task,
-            description: action.description,
-            categoryId: action.categoryId,
-          }
-        : task
-    );
-  }
-  if (action.type === TOGGLE_TASK) {
-    return state.map((task) =>
-      task.id === action.id
-        ? {
-            ...task,
-            done: !task.done,
-          }
-        : task
-    );
-  }
-  if (action.type === REMOVE_TASK) {
-    return state.filter((task) => task.id !== action.id);
-  }
-  return state;
 }
